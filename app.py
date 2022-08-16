b563820afda1388541443cccbc1b9797dc60d788
Luiza B. Laquini <72242547+luizalaquini@users.noreply.github.com>
2022-08-16 10:33:57 -0300

    G1 F400

--- app.py
+++ app.py
@@ -62,7 +62,7 @@ def createFormsGcode(form, cuttingSpeed, fontSize):
             path = path[3:]
             offsetGcode(path, offsetList[i][0], offsetList[i][1])
             finalGcode += path
-    finalGcode[0] = 'G1 F200' 
+    finalGcode[0] = 'G1 F400' 
     finalGcode = homing + configs + finalGcode
     return finalGcode
 
