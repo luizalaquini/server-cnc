cd3173e93410681d5bd7c8739104e9e9d86bc014
Luiza B. Laquini <72242547+luizalaquini@users.noreply.github.com>
2022-09-06 15:27:20 -0300

    NEW OFFSETS OK

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from TextToGcode import ttg
 # Conversão do texto para Gcode:
 fontScale = 0.5
 offsetList = [
-    (30, 79), # MODELO
-    (35, 72), # NUM SERIE
-    (97, 72), # CAP DE CARGA
-    (41, 64), # DATA DE FAB MES
-    (54, 64), # DATA DE FAB ANO
-    (106, 64), # CLASSE DE UTIL
-    (48, 55), # ESTADO DE CARGA
-    (77, 55), # PESO 
-    (42, 47), # POT INSTALADA
-    (97, 47), # VOLTAGEM
-    (50, 40)  # N CREA
+    (30, 78), # MODELO
+    (35, 70), # NUM SERIE
+    (95, 70), # CAP DE CARGA
+    (41, 62), # DATA DE FAB MES
+    (54, 62), # DATA DE FAB ANO
+    (106, 62), # CLASSE DE UTIL
+    (46, 54), # ESTADO DE CARGA
+    (77, 54), # PESO 
+    (42, 45), # POT INSTALADA
+    (94, 45), # VOLTAGEM
+    (45, 36)  # N CREA
 ]
 configs = [
     '$100=629.921', '$101=629.921', '$102=629.921', 
@@ -25,7 +25,7 @@ configs = [
     ]
 homing = ['$H', 'G92 X0 Y0 Z0']
 zUP = 'G1 F450 Z28'
-zDown = 'G1 F200 Z32.5'
+zDown = 'G1 F200 Z32.3'
  
 def offsetGcode(gcodeAbsolute, offsetX, offsetY):
     for i, command in enumerate(gcodeAbsolute):
